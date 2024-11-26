654bc873745b7cfb56255beb17fb1369b72a7648
dongjoon <dongjoon63@gmail.com>
2024-11-26 19:01:16 +0900

    커밋 테스트

--- section03/Ex03-1-escape.py
+++ section03/Ex03-1-escape.py
@@ -10,5 +10,29 @@
     \' : 작은 따옴표
     \" : 큰 따옴표
     \n : 개행(줄바꿈)
-
+    \\ : 백 슬래쉬
 '''
+
+#1. 이스케이프 문자 활용
+pokemon_info = 'ID: \'피카츄\'\n타입: \'전기\'\tLevel: 25'
+print(pokemon_info)
+
+#2. 경로 표시
+file_path = 'C:\\Program Files\\Git'
+print('폴더 경로:', file_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

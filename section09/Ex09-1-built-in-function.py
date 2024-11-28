b360d6bad8edc73420da3b4261ad9361d850c68b
dongjoon <dongjoon63@gmail.com>
2024-11-28 21:48:57 +0900

    커밋 테스트

---
+++ section09/Ex09-1-built-in-function.py
@@ -0,0 +1,60 @@
+'''
+
+파일명:Ex09-1-built-in-function
+
+내장함수
+    파이썬에서 제공해주는 별도의 import 없이
+    사용할 수 있는 함수
+
+'''
+# 예제1 : 리스트와 관련된 내장 함수등
+number = [1, -5, 2, 3, -8, 3, 9, -3, 4, 7, -1]
+print('1. 합계:', sum(number))
+print('2. 최대값:', max(number))
+print('3. 최솟값:', min(number))
+print('4-1. 정렬된 리스트', sorted(number))
+print('4-2. 역정렬 리스트', sorted(number, reverse=True))
+print('5-1. 절댓값 맵핑:', list(map(abs, number)))  # map(적용할 함수, 반복가능한 객체)
+print('5-2. 절댓값 함수:', abs(number[1]))
+print('6. 리스트 길이:', len(number))
+
+'''map이란 
+a_list = []
+for n in number
+    a_list.append(abs(n))
+'''
+
+# 예제2: 문자열과 관련된 함수들
+text = "Python Programming 123"
+words = ["apple", "banana", "cherry", "date"]
+str_alpha = "ABC"
+str_numeric = "123"
+
+print("1-1. 문자열이 알파벳인가?:", text.isalpha())        # isalpha()
+print("1-2. 문자열이 숫자인가?:", text.isnumeric())        # isnumeric()
+print("2. 대문자로:", text.upper())                     # upper()
+print("3. 소문자로:", text.lower())                     # lower()
+print("4. 문자열 분할:", text.split())                  # split()
+print("5. ASCII 코드:", ord('A'))                      # ord()
+print("6. ASCII to 문자:", chr(65))                    # chr()
+print("7-1. 문자열 위치:", text.find("Pro"))             # find()
+print("7-2. 문자열 위치:", text.index("Pro"))            # index() - 없으면 에러
+print("8. 문자 개수:", text.count('P'))                # count()
+print("9. 공백 제거:", text.strip())                   # strip()
+print("10. 문자열 합치기:", " ".join(words))           # join()
+
+# .join 같은 애들은 객체의 메소드 ord처럼 색바귀는게 내장함수
+
+#예제 3: 수학과 관련된 함수들
+
+
+
+
+
+
+
+
+
+
+
+

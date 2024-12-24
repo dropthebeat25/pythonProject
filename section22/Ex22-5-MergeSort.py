2bbf0bc9966d6a8c7bbf830320b2f4bffaf37f64
dongjoon <dongjoon63@gmail.com>
2024-12-24 21:45:01 +0900

    커밋 테스트

---
+++ section22/Ex22-5-MergeSort.py
@@ -0,0 +1,36 @@
+"""
+파일명: 22-5-MergeSort
+
+병합정렬(Merge Sort)
+    분할 정복 알고리즘의 일종으로, 리스트를 절반으로 나눈 후
+    각각을 재귀적으로 합병 정렬하고, 다시 합치면서 정렬하는 알고리즘
+
+"""
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
+
+arr = [5, 2, 8, 6, 1, 9, 3, 7]
+sorted_arr = merge_sort(arr)
+print(sorted_arr)

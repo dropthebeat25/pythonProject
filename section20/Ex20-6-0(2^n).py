a4e646826b6884bd40c715adbd8247d6aad9cb7b
dongjoon <dongjoon63@gmail.com>
2024-12-24 20:05:56 +0900

    커밋 테스트

---
+++ section20/Ex20-6-0(2^n).py
@@ -0,0 +1,34 @@
+"""
+파일명: Ex20-6-0(2^n)
+
+(2^n)
+    지수 시간 복잡도, 피보나치 수열처럼 재귀적 알고리즘
+
+"""
+
+def fibonacci(n):
+    if n <= 1:
+        return n
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(900))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
